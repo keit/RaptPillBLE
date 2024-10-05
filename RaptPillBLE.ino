9d8ca465f9086537b7306f3b760e4210b6b6d1c7
Kei Takashima <kei.takashima@crssoftware.co.nz>
2024-10-05 20:28:47 +1300

    use callback

--- RaptPillBLE.ino
+++ RaptPillBLE.ino
@@ -31,66 +31,68 @@ void setup() {
     while (1);
   }
 
-  Serial.println("Bluetooth® Low Energy Central scan");
+  Serial.println("Bluetooth® Low Energy Central scan callback");
+
+  // set the discovered event handle
+  BLE.setEventHandler(BLEDiscovered, bleCentralDiscoverHandler);
 
   // start scanning for peripheral
   BLE.scanForAddress(broadcasterAddress, false);
 }
 
-void loop() {
+void bleCentralDiscoverHandler(BLEDevice peripheral) {
   RaptPillDataV2 pillData;
 
-  // check if a peripheral has been discovered
-  BLEDevice peripheral = BLE.available();
-  // if (peripheral && peripheral.address().equalsIgnoreCase(broadcasterAddress)) {
-  if (peripheral) {
-    // discovered a peripheral
-    Serial.println("Discovered a peripheral");
-    Serial.println("-----------------------");
-    
-    // print address
-    Serial.print("Address: ");
-    Serial.println(peripheral.address());
-    Serial.print("Device name: ");
-    Serial.println(peripheral.deviceName());
-    Serial.print("Device local name: ");
-    Serial.println(peripheral.localName());
+  // discovered a peripheral
+  Serial.println("Discovered a peripheral");
+  Serial.println("-----------------------");
+  
+  // print address
+  Serial.print("Address: ");
+  Serial.println(peripheral.address());
+  Serial.print("Device name: ");
+  Serial.println(peripheral.deviceName());
+  Serial.print("Device local name: ");
+  Serial.println(peripheral.localName());
 
-    Serial.print("RSSI: ");
-    Serial.println(peripheral.rssi());
+  Serial.print("RSSI: ");
+  Serial.println(peripheral.rssi());
 
-    if (peripheral.hasManufacturerData()) {
-      int len = peripheral.manufacturerDataLength();
-      uint8_t manufactureData[len];
-      peripheral.manufacturerData(manufactureData, len);
+  if (peripheral.hasManufacturerData()) {
+    int len = peripheral.manufacturerDataLength();
+    uint8_t manufactureData[len];
+    peripheral.manufacturerData(manufactureData, len);
 
-      // for (int i = 0; i < len; i++) {
-      //   Serial.println(manufactureData[i], HEX);
-      //   Serial.println(" ");
-      // }
-      if (parseRaptPillDataV2(manufactureData, len, pillData)) {
-        blink();
-        Serial.print("Gravity Velocity: ");
-        Serial.println(pillData.gravityVelocity);
-        Serial.print("Temperature (C): ");
-        Serial.println(pillData.temperature);
-        Serial.print("Specific Gravity: ");
-        Serial.println(pillData.specificGravity);
-        Serial.print("Accel X: ");
-        Serial.println(pillData.accelX);
-        Serial.print("Accel Y: ");
-        Serial.println(pillData.accelY);
-        Serial.print("Accel Z: ");
-        Serial.println(pillData.accelZ);
-        Serial.print("Battery Percentage: ");
-        Serial.println(pillData.batteryPercentage);      
-      } else {
-        Serial.println("Failed to parse RAPT Pill data");
-      }
-      Serial.print("Free memory: ");
-      Serial.println(freeMemory());
+    // for (int i = 0; i < len; i++) {
+    //   Serial.println(manufactureData[i], HEX);
+    //   Serial.println(" ");
+    // }
+    if (parseRaptPillDataV2(manufactureData, len, pillData)) {
+      blink();
+      Serial.print("Gravity Velocity: ");
+      Serial.println(pillData.gravityVelocity);
+      Serial.print("Temperature (C): ");
+      Serial.println(pillData.temperature);
+      Serial.print("Specific Gravity: ");
+      Serial.println(pillData.specificGravity);
+      Serial.print("Accel X: ");
+      Serial.println(pillData.accelX);
+      Serial.print("Accel Y: ");
+      Serial.println(pillData.accelY);
+      Serial.print("Accel Z: ");
+      Serial.println(pillData.accelZ);
+      Serial.print("Battery Percentage: ");
+      Serial.println(pillData.batteryPercentage);      
+    } else {
+      Serial.println("Failed to parse RAPT Pill data");
     }
+    Serial.print("Free memory: ");
+    Serial.println(freeMemory());
   }
+}
+
+void loop() {
+  BLE.poll();
 }
 
 extern "C" char *sbrk(int incr);
